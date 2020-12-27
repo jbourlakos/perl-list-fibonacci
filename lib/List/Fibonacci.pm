5c4b9d95673e83601768cf76f84b435eb0664b6b
Ioannis Bourlakos <jbourlakos@gmail.com>
2020-12-28 01:28:38 +0200

    Initial commit

---
+++ lib/List/Fibonacci.pm
@@ -0,0 +1,105 @@
+package List::Fibonacci;
+
+use 5.006;
+use strict;
+use warnings;
+
+=head1 NAME
+
+List::Fibonacci - The great new List::Fibonacci!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use List::Fibonacci;
+
+    my $foo = List::Fibonacci->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 SUBROUTINES/METHODS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Ioannis Bourlakos, C<< <jbourlakos at gmail.com> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-list-fibonacci at rt.cpan.org>, or through
+the web interface at L<https://rt.cpan.org/NoAuth/ReportBug.html?Queue=List-Fibonacci>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc List::Fibonacci
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker (report bugs here)
+
+L<https://rt.cpan.org/NoAuth/Bugs.html?Dist=List-Fibonacci>
+
+=item * CPAN Ratings
+
+L<https://cpanratings.perl.org/d/List-Fibonacci>
+
+=item * Search CPAN
+
+L<https://metacpan.org/release/List-Fibonacci>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+This software is Copyright (c) 2020 by Ioannis Bourlakos.
+
+This is free software, licensed under:
+
+  The Artistic License 2.0 (GPL Compatible)
+
+
+=cut
+
+1; # End of List::Fibonacci
